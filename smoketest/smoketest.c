ae095cf62817583525232b9525bfb2c875b27900
Joseph Birr-Pixton <jpixton@gmail.com>
2016-01-17 00:07:25 +0000

    fix whitespace

--- smoketest/smoketest.c
+++ smoketest/smoketest.c
@@ -18,60 +18,60 @@ int main(void)
   const uint8_t *wrong_password = (const uint8_t *) "spanglypants";
 
   /* Setup enclave */
-	sgx_enclave_id_t eid;
-	sgx_status_t ret;
-	sgx_launch_token_t token = { 0 };
-		 
-	int token_updated = 0;
+  sgx_enclave_id_t eid;
+  sgx_status_t ret;
+  sgx_launch_token_t token = { 0 };
+     
+  int token_updated = 0;
 
-	uint8_t blob[PWENCLAVE_MAX_BLOB_SIZE] = { 0 };
+  uint8_t blob[PWENCLAVE_MAX_BLOB_SIZE] = { 0 };
 
-	ret = sgx_create_enclave(_T("pwenclave.signed.dll"), SGX_DEBUG_FLAG, &token, &token_updated, &eid, NULL);
-	if (ret != SGX_SUCCESS)
-	{
-		printf("sgx_create_enclave failed: %#x\n", ret);
-		return 1;
-	}
-	
+  ret = sgx_create_enclave(_T("pwenclave.signed.dll"), SGX_DEBUG_FLAG, &token, &token_updated, &eid, NULL);
+  if (ret != SGX_SUCCESS)
+  {
+    printf("sgx_create_enclave failed: %#x\n", ret);
+    return 1;
+  }
+  
   /* -- Setup password verified -- */
   time = GetTickCount();
   ret = pw_setup(eid, &pwerr, password, strlen(password), blob, sizeof blob, &bloblen);
   printf("pw_setup took %ums\n", GetTickCount() - time);
-	if (ret != SGX_SUCCESS)
-	{
-		printf("sgx pw_setup failed: %#x\n", ret);
+  if (ret != SGX_SUCCESS)
+  {
+    printf("sgx pw_setup failed: %#x\n", ret);
     sgx_destroy_enclave(eid);
-		return 1;
-	}
+    return 1;
+  }
 
   if (pwerr != PW_OK)
   {
-		printf("pw_setup reported failure: %#x\n", pwerr);
+    printf("pw_setup reported failure: %#x\n", pwerr);
     sgx_destroy_enclave(eid);
-		return 1;
+    return 1;
   }
 
   printf("setup worked, blob is %u bytes\n", bloblen);
-	for (i = 0 ; i < bloblen; i++)
-		printf("%02x", blob[i]);
-	printf("\n");
+  for (i = 0 ; i < bloblen; i++)
+    printf("%02x", blob[i]);
+  printf("\n");
 
   /* -- Check it works -- */
   time = GetTickCount();
   ret = pw_check(eid, &pwerr, password, strlen(password), blob, bloblen);
   printf("pw_check+ took %ums\n", GetTickCount() - time);
   if (ret != SGX_SUCCESS)
-	{
-		printf("sgx pw_check failed: %#x\n", ret);
+  {
+    printf("sgx pw_check failed: %#x\n", ret);
     sgx_destroy_enclave(eid);
-		return 1;
-	}
+    return 1;
+  }
 
   if (pwerr != PW_OK)
   {
-		printf("pw_check reported failure: %#x\n", pwerr);
+    printf("pw_check reported failure: %#x\n", pwerr);
     sgx_destroy_enclave(eid);
-		return 1;
+    return 1;
   }
 
   printf("pw_check worked (positive case)\n");
@@ -81,29 +81,29 @@ int main(void)
   ret = pw_check(eid, &pwerr, wrong_password, strlen(wrong_password), blob, bloblen);
   printf("pw_check- took %ums\n", GetTickCount() - time);
   if (ret != SGX_SUCCESS)
-	{
-		printf("sgx pw_check failed: %#x\n", ret);
+  {
+    printf("sgx pw_check failed: %#x\n", ret);
     sgx_destroy_enclave(eid);
-		return 1;
-	}
+    return 1;
+  }
 
   if (pwerr != PW_GUESS_WRONG)
   {
-		printf("pw_check reported wrong error: %#x\n", pwerr);
+    printf("pw_check reported wrong error: %#x\n", pwerr);
     sgx_destroy_enclave(eid);
-		return 1;
+    return 1;
   }
 
   printf("pw_check worked (negative case)\n");
   
-  /* Destroy enclave */	
-	ret = sgx_destroy_enclave(eid);
-	if (ret != SGX_SUCCESS)
-	{
-		printf("sgx_destroy_enclave failed: %#x\n", ret);
-		return 1;
-	}
+  /* Destroy enclave */  
+  ret = sgx_destroy_enclave(eid);
+  if (ret != SGX_SUCCESS)
+  {
+    printf("sgx_destroy_enclave failed: %#x\n", ret);
+    return 1;
+  }
 
   getchar();
-	return 0;
+  return 0;
 }
